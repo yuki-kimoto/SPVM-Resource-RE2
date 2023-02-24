e9eaa5bfd0d34d8841dd16e8b9630175360cc2b4
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-24 15:23:29 +0900

    Added all C++ header to my.cc

--- lib/SPVM/Resource/RE2.native/src/re2/my.cc
+++ lib/SPVM/Resource/RE2.native/src/re2/my.cc
@@ -1,4 +1,41 @@
+#include <algorithm>
+#include <assert.h>
 #include <atomic>
+#include <ctype.h>
+#include <deque>
+#include <functional>
+#include <immintrin.h>
+#include <iosfwd>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <ostream>
+#include <pthread.h>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <string_view>
+#include <type_traits>
+#include <unistd.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#ifdef _WIN32
+  #include <intrin.h>
+  #include <windows.h>
+#endif
 
 std::atomic<int> cnt;
 
