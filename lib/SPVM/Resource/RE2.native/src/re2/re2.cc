01f7f9895e1b705f7386999c12ecfba931eb2751
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-26 07:43:30 +0900

    Reduced

--- lib/SPVM/Resource/RE2.native/src/re2/re2.cc
+++ lib/SPVM/Resource/RE2.native/src/re2/re2.cc
@@ -1,21 +1,10 @@
 #include <sstream>
 
-class LogMessage {
- public:
-  LogMessage() {
-  
-  }
-  std::ostream& stream() { return str_; }
-
- private:
-  std::ostringstream str_;
-};
-
 namespace re2 {
 
 // Deletes this object; ref count has count reached 0.
 void Destroy() {
-  LogMessage().stream();
+  std::ostringstream str_;
 }
 
 }  // namespace re2
