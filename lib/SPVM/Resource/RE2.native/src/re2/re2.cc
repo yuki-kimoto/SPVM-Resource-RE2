beb5436f34da584afd32686989366acb6c931173
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-26 08:16:23 +0900

    Use strstream

--- lib/SPVM/Resource/RE2.native/src/re2/re2.cc
+++ lib/SPVM/Resource/RE2.native/src/re2/re2.cc
@@ -1,10 +1,10 @@
-#include <sstream>
+#include <strstream>
 
 namespace re2 {
 
 // Deletes this object; ref count has count reached 0.
 void Destroy() {
-  std::stringstream string; 
+  std::strstream string; 
 }
 
 }  // namespace re2
