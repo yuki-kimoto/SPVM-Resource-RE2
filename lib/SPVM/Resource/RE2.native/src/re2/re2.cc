b37dd735086a940db8a5f85e3924b8df6e622312
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-26 07:32:36 +0900

    Reduced

--- lib/SPVM/Resource/RE2.native/src/re2/re2.cc
+++ lib/SPVM/Resource/RE2.native/src/re2/re2.cc
@@ -10,31 +10,18 @@
 
 class LogMessage {
  public:
-  LogMessage(const char* file, int line)
-      : flushed_(false) {
+  LogMessage(const char* file, int line) {
     stream() << file << ":" << line << ": ";
   }
   void Flush() {
-    stream() << "\n";
     std::string s = str_.str();
-    size_t n = s.size();
-    if (fwrite(s.data(), 1, n, stderr) < n) {}  // shut up gcc
-    flushed_ = true;
-  }
-  ~LogMessage() {
-    if (!flushed_) {
-      Flush();
-    }
   }
   std::ostream& stream() { return str_; }
 
  private:
-  bool flushed_;
   std::ostringstream str_;
 };
 
-// Silence "destructor never returns" warning for ~LogMessageFatal().
-// Since this is a header file, push and then pop to limit the scope.
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable: 4722)
