6791214e69d442779c758ce0ed5f4f79d96a57dd
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-03-02 09:02:48 +0900

    Output WINVER

--- t/lib/SPVM/TestCase/Resource/RE2.cpp
+++ t/lib/SPVM/TestCase/Resource/RE2.cpp
@@ -13,6 +13,10 @@ int32_t SPVM__TestCase__Resource__RE2__test(SPVM_ENV* env, SPVM_VALUE* stack) {
     return 0;
   }
   
+#if defined(WINVER)
+  fprintf(stderr, "WINVER %d\n", WINVER);
+#endif
+  
   stack[0].ival = 1;
   
   return 0;
